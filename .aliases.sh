162da19e06d93d9b851f40916e887d47be3d1d69
Dima Baranov <vajnoe-dima@mail.ru>
2018-07-27 12:36:00 +0300

    add ssh functions and add comments about my asliases

--- .aliases.sh
+++ .aliases.sh
@@ -1,4 +1,8 @@
+#Show ip of all devices connected to my network
 alias SCANNET='nmap -sP 192.168.1.0/24'
+#Show files permissions
 alias ll='ls -ltr'
+#Clear screen
 alias cls='clear'
-
+#Check git status
+alias gs='git status'
--- .functions.sh
+++ .functions.sh
@@ -1,4 +1,14 @@
 
+#ADDING SSH KEY TO SSH SERVER
+#Fisrt arg is path to ssh key
+#Second arg is ssh uname@ip.adress
 function ADD_SSH_KEY() {
     cat $1 | ssh $2 "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys"
 }
+
+#ADDING MY SSH KEY TO SSH SERVER
+#ARG - ssh server adress
+function COPY_SSH() {
+    ssh-copy-id $@
+}
+
